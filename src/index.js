c6089df9efcbe19147a24a995d1f37af6e2dbdb2
Adam Bourn <abourn@uw.edu>
2016-11-29 12:41:04 -0800

    config, install, import firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyDYIT2x9mIRDK9_DPwtv2-B-wCAinfYrvU",
+  authDomain: "final-727e2.firebaseapp.com",
+  databaseURL: "https://final-727e2.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "834057190458"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
