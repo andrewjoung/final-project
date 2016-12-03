98015ec82d1e0207b278be901d22ad33a46d5e7a
Dominick Tavitian <dominicktavitian@gmail.com>
2016-12-02 16:23:05 -0800

    add filter bar and styleing for filtering by community

--- src/StoriesPage.js
+++ src/StoriesPage.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import firebase from 'firebase'; 
+import firebase from 'firebase';
+import { Button, IconButton, Menu, MenuItem } from 'react-mdl';
 
 class StoriesPage extends React.Component {
   constructor(props) {
@@ -11,33 +12,47 @@ class StoriesPage extends React.Component {
     }
     this.componentDidMount = this.componentDidMount.bind(this);
   }
-  
+
   componentDidMount() {
     var thisComponent = this;
     var storiesRef = firebase.database().ref('stories/');
     storiesRef.on('value', (snapshot) => {
       var storiesArray = [];
-      snapshot.forEach(function(childSnapshot) {
+      snapshot.forEach(function (childSnapshot) {
         var storyObj = childSnapshot.val();
         storyObj.key = childSnapshot.key;
         storiesArray.push(storyObj);
       });
-      thisComponent.setState({stories: storiesArray})
+      thisComponent.setState({ stories: storiesArray })
     });
   }
-  
+
   componentWillUnMount() {
     firebase.database().ref('stories/').off();
   }
 
   render() {
-    var storiesArray = this.state.stories.map(function(story) {
+    var storiesArray = this.state.stories.map(function (story) {
       console.log(story);
       return <Story content={story.content} />
     });
     //console.log(this.state);
     console.log(storiesArray);
-    return (<div>{storiesArray}</div>);
+    return (
+      <div>
+        <div className = "navWrap" >
+          <Button raised ripple className="button">Most Recent</Button>
+          <Button raised ripple className="button">Most Liked</Button>
+          <Button raised ripple className="button">Black</Button>
+          <Button raised ripple className="button">Latino</Button>
+          <Button raised ripple className="button">Muslim</Button>
+          <Button raised ripple className="button">LGBTQ</Button>
+          <Button raised ripple className="button">Other</Button>
+        </div>
+
+        <div>{storiesArray}</div>
+
+      </div>);
   }
 }
 
@@ -46,7 +61,7 @@ class StoriesPage extends React.Component {
 // also include the ability for the user to read more stories....
 class Story extends React.Component {
   render() {
-    return(<p>{this.props.content}</p>);
+    return (<p>{this.props.content}</p>);
   }
 }
 
